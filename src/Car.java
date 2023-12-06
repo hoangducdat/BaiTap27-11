5142d620780a7e0b225fe18c653a1aa5ca6356dc
hoangducdat <hoangducdat12@gmail.com>
2023-12-06 19:36:55 +0700

    baitapGit

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Car {
-}
+public class Car {
+        private String make, model;
+        private int year;
+        private boolean engineStatus;
+
+        public Car(String make, String model, int year) {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.engineStatus = false;
+        }
+
+        public void startEngine() {
+            engineStatus = true;
+            System.out.println("Động cơ đã được khởi động.");
+        }
+
+        public void stopEngine() {
+            engineStatus = false;
+            System.out.println("Động cơ đã được tắt.");
+        }
+
+        public void displayCarInfo() {
+            System.out.println("Thông tin xe: " + make + " " + model + " " + year + ", Trạng thái động cơ: " + (engineStatus ? "Đang chạy" : "Đã tắt"));
+        }
+
+        public static void main(String[] args) {
+            Car myCar = new Car("Toyota", "Camry", 2022);
+            myCar.displayCarInfo();
+            myCar.startEngine();
+            myCar.displayCarInfo();
+            myCar.stopEngine();
+            myCar.displayCarInfo();
+        }
+    }
+
+
